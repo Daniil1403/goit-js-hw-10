cc3599572859ce08be6b2e570dde751ec7ee3759
Daniil1403 <104705712+Daniil1403@users.noreply.github.com>
2022-10-16 09:54:13 +0300

    Update files

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,23 +1,12 @@
-const URL = 'https://restcountries.com/v3.1/name';
+const URL = 'https://restcountries.com/v2/name';
 
 export function fetchCountries(name) {
   return fetch(
     `${URL}/${name}?fields=name,capital,population,flag,languages`
-  ).then(res => res.json());
+  ).then(res => {
+    if (!res.ok) {
+      throw new Error('Oops, there is no country with that name');
+    }
+    return res.json();
+  });
 }
-
-// const URL = 'https://restcountries.com/v3.1/name';
-
-// export function fetchCountries(name) {
-//   return fetch(`${URL}/${name}?fields=name,capital,population,flags,languages`)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error();
-//       }
-//       return response.json();
-//     })
-//     .catch(error =>
-//       // добавить всплывающее сообщение /404?/
-//       console.log(error)
-//     );
-// }
